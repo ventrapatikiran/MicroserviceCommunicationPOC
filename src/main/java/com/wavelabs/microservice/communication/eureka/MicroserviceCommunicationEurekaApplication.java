bb5922947c4a3982a0626ad0b6a88c5d1315a426
kiran.kumar <kiran.kumar@care.com>
2019-06-24 23:06:29 +0530

    Zuul proxy inital commit

--- src/main/java/com/wavelabs/microservice/communication/eureka/MicroserviceCommunicationEurekaApplication.java
+++ src/main/java/com/wavelabs/microservice/communication/eureka/MicroserviceCommunicationEurekaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableZuulProxy
 public class MicroserviceCommunicationEurekaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceCommunicationEurekaApplication.class, args);
